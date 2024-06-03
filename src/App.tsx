ab91fe971b9a1f84a1a7b77e1348f823f74bc4db
sondang <sondang2kk@gmail.com>
2024-06-03 16:42:14 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+
+import { Route, Routes, useLocation } from "react-router-dom";
+import Loader from "./common/Loader";
+import PageTitle from "./components/PageTitle";
+import { Dashboard } from "./pages/Dashboard";
+
+function App() {
+  const [loading, setLoading] = useState<boolean>(true);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+  return loading ? (
+    <Loader />
+  ) : (
+    <>
+      <Routes>
+        <Route
+          index
+          element={
+            <>
+              <PageTitle title="eCommerce Dashboard | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <Dashboard />
+            </>
+          }
+        />
+
+        {/* <Route
+          path="/auth/signin"
+          element={
+            <>
+              <PageTitle title="Signin | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <SignIn />
+            </>
+          }
+        /> */}
+        {/* <Route
+          path="/auth/signup"
+          element={
+            <>
+              <PageTitle title="Signup | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <SignUp />
+            </>
+          }
+        /> */}
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import DefaultLayout from "../../layout/DefaultLayout";
+
+export const Dashboard: React.FC = () => {
+  return (
+    <DefaultLayout>
+      <div>
+        <p>hello</p>
+      </div>
+    </DefaultLayout>
+  );
+};
